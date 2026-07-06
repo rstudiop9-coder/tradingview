598defb72feb9b7c95c8542896c49efd86d051aa
rstudiop9-coder <rstudiop9@gmail.com>
2026-07-06 12:45:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from tv_get_csv import generate_csv
 
 #inputs
 symbol_name="MCX:GOLD1!"
-username = 'your_emial id or user name'
-password = 'trading view password'
+username = 'rstudiop9@gmail.com'
+password = 'Glady@2001'
 
 # symbol_name="NSE:NIFTY1!"
 frequency="1_day" #"1_minute", "1_day" , "1_week", "1_month"
